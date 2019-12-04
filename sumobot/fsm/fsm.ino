68b6230c2c1999b75a8ac0a81da7d7422e4e2797
eda214 <edward_athaide@college.harvard.edu>
2019-12-04 15:36:25 -0500

    FSM partially written and worked on getting TOF readings

--- sumobot/fsm/fsm.ino
+++ sumobot/fsm/fsm.ino
@@ -12,10 +12,14 @@
 
 // Threshold for whether refl sensor detects a line (placeholder number for now)
 #define REF_THRESHOLD 0
+// Threshold for TOF (also placeholder)
+#define TOF_THRESHOLD 100
 
+// States
 enum FsmState {Stopped, Fwd, Rev, Left, Right};
 FsmState cur_state;
 
+// I^2C stuff
 void init_sensor_(uint8_t index);
 
 const int num_tof_sensors = 2;
@@ -29,7 +33,8 @@ bool edgeFR();
 bool edgeRL();
 bool edgeRR();
 
-bool opntInFront();
+bool tofLeft();
+bool tofRight();
 
 void setup() {
     // Change pin numbers in hardware_config.hpp
@@ -47,33 +52,36 @@ void setup() {
     // Starting at stopped
     cur_state = Stopped;
 
-     for (uint8_t i = 0; i < num_tof_sensors; i++) {
-         pinMode(sensor_pins_[i], OUTPUT);
-         digitalWrite(sensor_pins_[i], LOW);
-     }
+    for (uint8_t i = 0; i < num_tof_sensors; i++) {
+        pinMode(sensor_pins_[i], OUTPUT);
+        digitalWrite(sensor_pins_[i], LOW);
+    }
   
-     delay(50);
-     Wire.begin();
+    delay(50);
+    Wire.begin();
   
-     // Set sensor addresses
-     for (uint8_t i = 0; i < num_tof_sensors; i++) {
-         digitalWrite(sensor_pins_[i], HIGH);
-         delay(50);
-         sensors_[i]->setAddress(2 * i);
-         // Uncomment to debug addresses of sensors
-         // Serial.println(sensors[i]->readReg(0x212));
-     }
-     delay(50);
+    // Set sensor addresses
+    for (uint8_t i = 0; i < num_tof_sensors; i++) {
+        digitalWrite(sensor_pins_[i], HIGH);
+        delay(50);
+        sensors_[i]->setAddress(2 * i);
+        // Uncomment to debug addresses of sensors
+        // Serial.println(sensors[i]->readReg(0x212));
+    }
+    delay(50);
   
-     // Initializes sensors
-     for (uint8_t i = 0; i < num_tof_sensors; i++) {
-         initSensor_(i);
-     }
+    // Initializes sensors
+    for (uint8_t i = 0; i < num_tof_sensors; i++) {
+        initSensor_(i);
+    }
 
     #if DEBUG_ENABLED
         Serial.begin(9600);
         Serial.println("Initialization complete.");
     #endif
+
+    // 5-second delay
+    delay(5000);
 }
 
 void loop() {
@@ -89,26 +97,58 @@ void loop() {
             digitalWrite(pins::motorR2, HIGH);
             digitalWrite(pins::motorL1, HIGH);
             digitalWrite(pins::motorL2, HIGH);
-            // If edge detected in front 
-            // Reverse?
-            // Else if opponent detected in front
-            // Forward
-            //if(edgeInFront()) {
-            //  cur_state = Rev;
-            //}
-            //else if (opntInFront()) {
-            //  cur_state = Fwd;
-            //}
+            // Edge in front -> reverse 
+            if(edgeFL() && edgeFR()) {
+              cur_state = Rev;
+            }
+            // Edge behind -> forward
+            else if (edgeRL() && edgeRR()) {
+              cur_state = Fwd;
+            }
+            // Edge on left -> turn right
+            else if (edgeFL() && edgeRL()) {
+              cur_state = Right;
+            }
+            // Edge on right -> turn left
+            else if (edgeFR() && edgeRR()) {
+              cur_state = Left;
+            }
+            // Edge in front corner -> turn away from edge
+            // NEED BETTER RESPONSE HERE
+            // Possibly more specific kinds of turns?
+            else if (edgeFL()) {
+              cur_state = Right;
+            }
+            else if (edgeFR()) {
+              cur_state = Left;
+            }
+            // Edge in rear corner -> turn away from edge
+            // Probably fine in most cases, but test
+            else if (edgeRL()) {
+              cur_state = Right;
+            }
+            else if (edgeRR()) {
+              cur_state = Left;
+            }
             // What else?
+            // Opponent in front cases
             break;
         case Fwd:
             digitalWrite(pins::motorR1, HIGH);
             digitalWrite(pins::motorR2, LOW);
             digitalWrite(pins::motorL1, HIGH);
             digitalWrite(pins::motorL2, LOW);
+            //PWM???
             // Switching Logic
-            //if (edgeInFront()) {
-            //  cur_state = Rev;
+            // If driving parallel to edge, turn away from it
+            if (edgeFL() && edgeRL()) {
+              cur_state = Right;
+            }
+            else if (edgeFR() && edgeRR()) {
+              cur_state = Left;
+            }
+            //if (edgeFL() || edgeFR()) {
+            //  cur_state = Stopped;
             //}
             break;
         case Rev:
@@ -154,9 +194,17 @@ bool edgeRR() {
   return (analogRead(pins::refRR) < REF_THRESHOLD);
 }
 
-// Should return true if opponent detected in front, false otherwise
-bool opntInFront() {
-  return false;
+// Should return true if something detected, false otherwise
+uint16_t getTOF(int i) {
+  return (*sensors_[i]).readRangeSingleMillimeters();
+}
+
+bool tofLeft() {
+  return (getTOF(0) < TOF_THRESHOLD);
+}
+
+bool tofRight() {
+  return (getTOF(1) < TOF_THRESHOLD);
 }
 
 void initSensor_(uint8_t index) {
@@ -167,40 +215,3 @@ void initSensor_(uint8_t index) {
    Serial.println(" online.");
    delay(200);
 }
-
-/*
- * sensors_ = {new VL53L0X, new VL53L0X, new VL53L0X};
-sensor_names_ = {“left”, “front”, right”};
-sensor_pins_= {XSHUT1, XSHUT2, XSHUT3};
-for (uint8_t i = 0; i < sensor_pins_.size(); i++) {
-       pinMode(sensor_pins_[i], OUTPUT);
-       digitalWrite(sensor_pins_[i], LOW);
-   }
-
-   delay(50);
-   Wire.begin();
-
-   // Set sensor addresses
-   for (uint8_t i = 0; i < sensor_pins_.size(); i++) {
-       digitalWrite(sensor_pins_[i], HIGH);
-       delay(50);
-       sensors_[i]->setAddress(2 * i);
-       // Uncomment to debug addresses of sensors
-       // Serial.println(sensors[i]->readReg(0x212));
-   }
-   delay(50);
-
-   // Initializes sensors
-   for (uint8_t i = 0; i < sensor_pins_.size(); i++) {
-       initSensor_(i);
- }
-
-initSensor_(uint8_t index) {
-   VL53L0X* sensor = sensors_[index];
-   sensor->init();
-   sensor->setTimeout(500);
-   Serial.print(sensor_names_[index]);
-   Serial.println(” online.“);
-   delay(200);
-}
- */
